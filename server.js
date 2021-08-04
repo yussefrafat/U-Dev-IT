69b6a94452e3b7d0e35f7de0cce63a2ae0ede968
Yussef Rafat <yussefrafat5908@gmail.com>
2021-08-04 18:31:43 -0400

    created cadidates

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const mysql = require('mysql2');
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +7,47 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Lille59000',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+//GET a single candidate
+ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+ if (err) {
+   console.log(err);
+  }
+console.log(row);
+ });
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+   if (err) {
+ console.log(err);
+  }
+    console.log(result);
+ });
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
